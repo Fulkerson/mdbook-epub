f14311c07c3b4361fa15be192aa5adfb05d6f6b2
Travis CI User <travis@example.org>
2018-01-07 09:37:29 +0000

    Documentation for Michael-F-Bryan/mdbook-epub@04f40a2

---
+++ implementors/error_chain/trait.ChainedError.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["epub_builder"] = ["impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"epub_builder/struct.Error.html\" title=\"struct epub_builder::Error\">Error</a>",];
+implementors["error_chain"] = [];
+implementors["mdbook"] = ["impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"mdbook/errors/struct.Error.html\" title=\"struct mdbook::errors::Error\">Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ mdbook/utils/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["render_markdown",""]],"mod":[["fs",""]]});
